465d184562f5d96956eac69e01e991e254e3992f
chenjj6843 <chenjj6843@yeastar.com>
2023-04-19 16:17:40 +0800

    完成gee框架到路由前缀树的开发

---
+++ gee/gee.go
@@ -0,0 +1,39 @@
+package gee
+
+import (
+	"net/http"
+)
+
+// HandlerFunc defines the request handler used by gee
+type HandlerFunc func(c *Context)
+
+// Engine implement the interface of ServeHTTP
+type Engine struct {
+	router *router
+}
+
+// New is the constructor of gee.Engine
+func New() *Engine {
+	return &Engine{router: newRouter()}
+}
+
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	engine.router.addRoute(method, pattern, handler)
+}
+
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c := newContext(w, req)
+	engine.router.handle(c)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module Project_Gee
+
+go 1.19
+
+require gee v0.0.0
+
+replace gee => ./gee
+
